3d275d533cf3d8549e6501fb9d618bd5ef26d5a5
cctv4-cgtn <69949010+cctv4-cgtn@users.noreply.github.com>
2023-11-11 17:04:44 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-git clone https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
+git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git package/luci-theme-kucat
