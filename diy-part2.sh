a6b3b8979a3938027f42a5d90013e34fd9b47a55
cctv4-cgtn <69949010+cctv4-cgtn@users.noreply.github.com>
2025-09-08 18:41:51 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone -b 18.06 --single-branch https://github.com/jerrykuku/luci-theme-argon.git package/downloads/luci-theme-argon-jerrykuku
